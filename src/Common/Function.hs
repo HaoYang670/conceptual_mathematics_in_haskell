be2eb95e24242b2c8a8a18b5d2068ee919089013
remzi <13716567376yh@gmail.com>
2023-03-06 20:15:32 +0800

    rename Map to Function
    Signed-off-by: remzi <13716567376yh@gmail.com>

--- src/Common/Function.hs
+++ src/Common/Function.hs
@@ -1,19 +1,28 @@
-module Common.Map where
+module Common.Function where 
 
 import Common.Set (Element, Set)
 import Common.Error (Result)
-import qualified Data.Map as DM
+import Data.Map (Map)
 
-type Relation = DM.Map Element Element
+type Relation = Map Element Element
 
 -- A total function from domain to codomain
 -- I don't find a way to implement the map lazily (such as function).
 -- I have to store all the maping relation now.
-data Map = Map{
+data Function = Map{
   domain :: Set,
   codomain :: Set,
   rule :: Relation
 }
+
+instance Show Function where
+  show m = "domain: " ++ show (domain m) ++
+    "\ncodomain: " ++ show (codomain m)
+
+instance Eq Function where
+  (==) m1 m2 = (domain m1 == domain m2) && (codomain m1 == codomain m2) && (rule m1 == rule m2)
+
+-- identity :: Set -> Function
 
 -- Try to build a Map. 
 -- build :: Set -> Set -> Relation -> Result Map
